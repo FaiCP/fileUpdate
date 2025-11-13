bf23f35eb69c9518c7bf446e974cadb072c6b05f
luis cadme <luisangel88776@gmail.com>
2025-11-13 07:10:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1251751551-85fed",
+  "appId": "1:212769320655:web:c26ba315ee81a8eca351ba",
+  "apiKey": "AIzaSyAjP5vquae_fTH_bcXgxc-R98olI-J0LL0",
+  "authDomain": "studio-1251751551-85fed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "212769320655"
+};
